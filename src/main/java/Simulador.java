b83e90623f961dfd995ecf25f8a67682123c89ca
car-cas <carlos.castaneda-l@mail.escuelaing.edu.co>
2019-10-03 19:12:38 -0500

    Programming Drill 3.2.2 y 3.3.2

--- src/main/java/Simulador.java
+++ src/main/java/Simulador.java
@@ -10,6 +10,7 @@ public class Simulador {
     public Complejo[][] getMatriz(){
         return matriz;
     }
+
     /**
      * Metodo programmingDrill311Y321, realiza la simulacion del programa 3.1.1 y 3.2.1 del libro.
      * Dada una matriz, un vector y unos determinados clicks determina el numero de
@@ -39,13 +40,51 @@ public class Simulador {
      * Metodo programmingDill322, realiza la simulacion del programa 3.2.2 del libro.
      * Permite el calculo de la matriz de probabilidades del experimento de doble rendija
      * segun la cantidad de rendijas igresadas y numero de objetivos.
-     * @param slits
-     * @param targets
-     * @param probabilidades
-     * @return
+     * @param matriz double[][]
+     * @param estadoInicial double[]
+     * @param slits int
+     * @return double[]
      */
-    public double [][] programmingDrill322(int slits, int targets, int [][] probabilidades){
-        return null;
+    public double[] programmingDrill322(double[][]matriz,double[]estadoInicial,int slits){
+        double [] respuesta = new double[estadoInicial.length];
+        while(slits!=0) {
+            for (int i = 0; i < estadoInicial.length; i++) {
+                double suma = 0;
+                for (int j = 0; j < estadoInicial.length; j++) {
+                    suma += matriz[i][j] * estadoInicial[j];
+                }
+                respuesta[i] = suma;
+            }
+            System.arraycopy(respuesta, 0, estadoInicial, 0, estadoInicial.length);
+            slits-=1;
+        }
+        return respuesta;
+    }
+
+    /**
+     * Metodo programmingDill332, realiza la simulacion del programa 3.3.2 del libro.
+     * Permite el calculo de la matriz de probabilidades del experimento de doble rendija
+     * segun la cantidad de rendijas igresadas y numero de objetivos con numeros complejos.
+     * @param slits
+     * @param matriz
+     * @param estadoInicial
+     * @return Complejo[]
+     */
+    public Complejo[] programmingDrill332(int slits,Complejo[][]matriz,Complejo[]estadoInicial){
+        Complejo[] respuesta = new Complejo[estadoInicial.length];
+        while (slits!=0){
+            for (int i = 0; i < estadoInicial.length ; i++) {
+                Complejo valor = new Complejo(0,0);
+                for (int j = 0; j < estadoInicial.length ; j++) {
+                    valor = Operaciones.suma(valor,Operaciones.producto(matriz[i][j],estadoInicial[j]));
+                }
+                respuesta[i]=valor;
+            }
+            System.arraycopy(respuesta, 0, estadoInicial, 0, estadoInicial.length);
+            slits-=1;
+
+        }
+        return respuesta;
     }
 
     /**
@@ -57,7 +96,7 @@ public class Simulador {
      * @param clicks int
      * @return Complejo[]
      */
-    public static Complejo[] programmingDrill331(Complejo[][]matriz,Complejo[]estadoInicial,int clicks){
+    public Complejo[] programmingDrill331(Complejo[][]matriz,Complejo[]estadoInicial,int clicks){
         Complejo[] respuesta = new Complejo[estadoInicial.length];
         while (clicks!=0){
             for (int i = 0; i < estadoInicial.length ; i++) {
--- src/test/java/SimuladorTest.java
+++ src/test/java/SimuladorTest.java
@@ -42,6 +42,21 @@ public class SimuladorTest{
     }
 
     @Test
+    public void  programmingDrill322Test(){
+        Simulador simulator = new Simulador();
+        double[][] matrizDoubles = {{0,0,0,0},
+                {0,0,0,2.3},
+                {0,2.3,0,0},
+                {2.3,0,0,0}};
+        double[] estadoInicial = {6,2,5,3};
+        double[] solucion = simulator.programmingDrill322(matrizDoubles, estadoInicial, 2);
+        double[] respuesta = {0,31.739999999999995,15.869999999999997,0};
+        for(int i=0;i<respuesta.length;i++){
+            assertTrue(respuesta[i]==solucion[i]);
+        }
+    }
+
+    @Test
     public void programmingDrill331Test(){
         Simulador simulator = new Simulador();
         Complejo[][] matrizComplejos = new Complejo[3][3];
@@ -65,4 +80,30 @@ public class SimuladorTest{
         respuesta[2]=new Complejo(-484,100);
         Assert.assertArrayEquals(solucion,respuesta);
     }
+
+    @Test
+    public void programmingDrill332Test(){
+        Simulador simulator = new Simulador();
+        Complejo[][] matrizComplejos = new Complejo[3][3];
+        matrizComplejos[0][0] = new Complejo(2,4);
+        matrizComplejos[0][1] = new Complejo(1,3);
+        matrizComplejos[0][2] = new Complejo(5,0);
+        matrizComplejos[1][0] = new Complejo(3,2);
+        matrizComplejos[1][1] = new Complejo(1,2);
+        matrizComplejos[1][2] = new Complejo(7,12);
+        matrizComplejos[2][0] = new Complejo(2,2);
+        matrizComplejos[2][1] = new Complejo(1,1);
+        matrizComplejos[2][2] = new Complejo(2,4);
+        Complejo[] estadoInicial = new Complejo[3];
+        estadoInicial[0]=new Complejo(1,3);
+        estadoInicial[1]=new Complejo(2,5);
+        estadoInicial[2]=new Complejo(3,6);
+        Complejo[] solucion = simulator.programmingDrill332(2,matrizComplejos,estadoInicial);
+        Complejo[] respuesta = new Complejo[3];
+        respuesta[0]=new Complejo(-701,177);
+        respuesta[1]=new Complejo(-1027,84);
+        respuesta[2]=new Complejo(-484,100);
+        Assert.assertArrayEquals(solucion,respuesta);
+    }
+
 }
